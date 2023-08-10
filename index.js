63882bf8949f5cea41635a175421068724229a35
Gunarks <florianomanoel@hotmail.com>
2023-08-10 09:50:41 -0300

    :tada: initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Function mode modify
+function toggleMode() {
+  const body = document.body
+  body.classList.toggle("light")
+
+  // Pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // Subistiuir a imagem
+  if (body.classList.contains("light")) {
+    // Se tiver light mode, adicionar a imagem ligth
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    // Se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
